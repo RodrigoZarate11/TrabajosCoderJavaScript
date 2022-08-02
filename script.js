19e49f8e42961b46a300472216ef748ae2210dee
Rodrigo Zarate <63462095+RodrigoZarate11@users.noreply.github.com>
2022-08-02 20:10:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,58 +1,24 @@
-/* ----------------------------------------------------------------------------------- */
-/* Imprime numeros del 1 al 100 en pantalla */
+let listaDePaises = document.getElementById('listaDePaises')
+let valorNuevoPais = document.getElementById('valorNuevoPais')
+let botonAgregar = document.getElementById('botonAgregar')
 
-/* let num = 1
+botonAgregar.addEventListener("click",agregarPais)
 
-do {
-    console.log(num)
-    num = num + 1
-} while (num<=100);
- */
+function agregarPais() {
+        let pais = document.createElement("h4")
+        pais.innerText = valorNuevoPais.value
+        if (validarPais(valorNuevoPais)== true) {
+            listaDePaises.append(pais)
+            valorNuevoPais.value = ''
+        } 
+    } 
+ 
 
-
-/*-----------------------------------------------------------------------------------*/
-/* Ejercio de validacion de usuario y contraseña, con 3 ingresos de contraseña incorrectos muestra cartel de generar una nueva*/
-
-let usuario = prompt("Ingrese usuario para registrarse")
-let constrasenia = prompt("Ingrese una contraseña para registrarse")
-let contador = 0
-
-
-var usuarioComparacion = prompt("Ingrese su usuario para ingresar al sitio")
-while(usuario!=usuarioComparacion){
-    alert("Usuario incorrecto, intente nuevamente")
-    usuarioComparacion = prompt("Ingrese su usuario")
+function validarPais(String){
+    let pais = ['Argentina' , 'Brasil' , 'Uruguay' , 'Paraguay','Colombia','Peru','Chile','Estados Unidos','Mexico','España','Italia','Alemania','Rusia','Inglaterra','Escocia','Sudafrica', 'Australia', 'China','Japon', 'Nueva Zelanda']
+    if ( pais.includes(valorNuevoPais.value)== true) {
+        return true
+    }else{
+        alert("Ese pais no esta registrado")
     }
-
-let contraseniaComparacion = prompt("Ingrese su contraseña para ingresar al sitio")
-while(constrasenia!=contraseniaComparacion && contador<3){
-    alert("contraseña incorrecta, intente nuevamente")
-    contador = contador + 1 
-    usuarioComparacion = prompt("Ingrese su contraseña")
-    }
-if(contador==3){
-    console.log("Usted ha ingresado erroneamente " ,contador ,"veces la contraseña, debe generarla nuevamente. Gracias")
-    constrasenia= "contraseñaInhackeableHastaNuevoTramite"
-    exit
 }
-console.log("Bienvenido, usted ha ingresado al sitio")
-
-/*-----------------------------------------------------------------------------------*/
-/*El factorial de un numero entero en matematicas es el producto de todos los números enteros positivos desde 1 hasta n
-Por ejemplo: 5! = 1  x  2  x  3  x  4 x  5= 120.*/
-
-/* function calcularFactorial(){
-
-    let factorial = parseInt(prompt("Ingrese un numero entero a calcular su factorial"))
-    let resultado = 1
-    for (let i = 1; i <= factorial; i++) {
-        resultado = i * resultado
-    
-    }
-    console.log("La factorial de",factorial ,"es: ",resultado)
-}
-
-calcularFactorial() */
-
-
-
